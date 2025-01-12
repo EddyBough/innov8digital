53252c77608c55cafcf39f4b6f32e4143591fbbd
EddyBough <boughanmieddy@outlook.fr>
2025-01-12 18:46:03 +0100

    modify: #95 first component blur

--- src/app/sections-home/FirstSection.tsx
+++ src/app/sections-home/FirstSection.tsx
@@ -4,26 +4,26 @@ import { Button } from "../../components/ui/button";
 
 export default function FirstSection() {
   return (
-    <section className="relative min-h-[90vh] w-full flex items-center justify-center overflow-hidden bg-white mt-9 md:mt-0 lg:mt-0">
+    <section className="relative min-h-[90vh] w-full flex items-center justify-center overflow-hidden bg-white mt-9 lg:mt-11 md:mt-0 py-16 md:py-0 lg:py-0">
       <Image
         src="/img/MainBlurBlue.svg"
         alt=""
         width={1100}
         height={150}
         priority
-        className="absolute h-[45rem] sm:h-[50rem] md:h-[60rem] lg:h-[68rem] left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 w-screen max-w-none lg:max-w-screen-2xl object-cover"
+        className="absolute h-[38rem] sm:h-[45rem] md:h-[56rem] lg:h-[68rem] left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 w-screen max-w-none lg:max-w-screen-2xl object-cover"
         quality={100}
       />
 
       {/* Conteneur principal avec padding ajusté */}
       <div className="relative z-20 max-w-6xl mx-auto text-center px-4 pb-24 pt-12">
         {/* Titre principal */}
-        <h1 className="text-4xl sm:text-5xl md:text-7xl font-bold text-white mb-8">
+        <h1 className="text-4xl sm:text-5xl md:text-7xl font-bold text-white mb-8 mt-3 lg:mt-0 md:mt-0">
           L&apos;agence digitale
         </h1>
 
         {/* Services */}
-        <p className="text-base sm:text-lg md:text-xl text-white max-w-3xl mx-auto mb-12 leading-relaxed">
+        <p className="text-base sm:text-lg md:text-xl text-white max-w-3xl mx-auto mb-9 leading-relaxed mt-9 lg:mt-0 md:mt-0">
           Site internet sur mesure - Applications mobiles - Intégration IA -
           Maintenance -
           <br className="hidden sm:block" />
