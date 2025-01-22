f20759cbe6ccb1927e4625e3a474236b5973844e
EddyBough <boughanmieddy@outlook.fr>
2025-01-22 16:12:39 +0100

    add: #158 favicon on layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,6 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { Navigation } from "../components/features/Navigation";
 import Footer from "../components/features/Footer";
-import React from "react";
 import localFont from "next/font/local";
 import CookieBanner from "@/components/CookieBanner";
 import Script from "next/script";
@@ -101,6 +100,25 @@ export default function RootLayout({
             `,
           }}
         />
+        {/* Favicons */}
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/site.webmanifest" />
       </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} ${aeonik.variable} antialiased`}
