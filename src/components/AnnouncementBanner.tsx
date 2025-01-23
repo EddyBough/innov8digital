6d0b08b6b137624a2cce182b041648292c0336a1
EddyBough <boughanmieddy@outlook.fr>
2025-01-23 10:55:48 +0100

    fix: #163 announcement banner too short on mobile

--- src/components/AnnouncementBanner.tsx
+++ src/components/AnnouncementBanner.tsx
@@ -1,18 +1,14 @@
 export default function AnnouncementBanner() {
   return (
     <div className="bg-black text-white py-3 shadow-lg overflow-hidden">
-      <div className="relative whitespace-nowrap flex lg:gap-x-[30rem] md:gap-x-96 items-center animate-marquee">
-        {/* Ajustement de l'espacement pour bureau */}
-        <p className="mx-8 text-white">
-          Jusqu&apos;à <span className="font-bold text-white">-30%</span> sur
-          toutes nos prestations pour le lancement ! Profitez-en jusqu&apos;au
-          28 Février 2025 !
-        </p>
-        <p className="mx-8 text-white">
-          Jusqu&apos;à <span className="font-bold text-white">-30%</span> sur
-          toutes nos prestations pour le lancement ! Profitez-en jusqu&apos;au
-          28 Février 2025 !
-        </p>
+      <div className="relative flex items-center whitespace-nowrap animate-marquee w-[300%] lg:w-[80%]">
+        {[...Array(3)].map((_, index) => (
+          <p key={index} className="mx-8 text-white">
+            Jusqu&apos;à <span className="font-bold">-30%</span> sur toutes nos
+            prestations pour le lancement ! Profitez-en jusqu&apos;au 28 Février
+            2025 !
+          </p>
+        ))}
       </div>
     </div>
   );
